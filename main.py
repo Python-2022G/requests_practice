4af135ba7da122cf391f2aac8080fa3e8a85e80c
djumanov <diyorbekjumanov1120@gmail.com>
2022-12-19 18:46:23 +0500

    params

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import requests
 from pprint import pprint
 
 URL = 'https://randommer.io/api/Card'
-
+payload = {
+    'type': 'Mastercard'
+}
 header = {
     'X-Api-Key': '2d794c6f46094ceb96bd719c1c26c984'
 }
 
-response = requests.get(URL, headers=header)
+response = requests.get(URL, params=payload, headers=header)
 
 print(response.url)
 pprint(response.json())
