575008f622bffd67a27ba53837c2bc74c43aaaf5
djumanov <diyorbekjumanov1120@gmail.com>
2022-12-19 17:38:56 +0500

    params

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import requests
 
 URL = 'https://randomuser.me/api/'
 payload = {
-    'results': 10,
-    'gender': 'male'
+    'results': 3,
+    'gender': 'male',
+    'nat': 'US'
 }
 
 response = requests.get(URL, params=payload)
 
 print(response.url)
+print(response.json())
