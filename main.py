08257b0e3bc5675284e6d37b2904d037da0becde
djumanov <diyorbekjumanov1120@gmail.com>
2022-12-19 19:58:44 +0500

    get name

--- main.py
+++ main.py
@@ -2,16 +2,23 @@ import requests
 from pprint import pprint
 
 URL = 'https://randommer.io/api/Name'
-payload = {
-    'nameType': 'fullname',
-    'quantity': 5
-}
-header = {
-    'X-Api-Key': '2d794c6f46094ceb96bd719c1c26c984'
-}
+KEY = '2d794c6f46094ceb96bd719c1c26c984'
 
-response = requests.get(URL, params=payload, headers=header)
+def get_name(name, n):
+    payload = {
+        'nameType': name,
+        'quantity': n
+    }
+    header = {
+        'X-Api-Key': KEY
+    }
 
-print(response.url)
-pprint(response.json())
+    response = requests.get(URL, params=payload, headers=header)
+    return response.json()
+
+name = 'fullname'
+n = 5
+names = get_name(name, n)
+
+print(names)
 
