5c7599f0748aff9a93307d912f4fa1bc377aac05
azimabdurasulov <azimjon20050604@gmail.com>
2022-12-22 14:41:23 +0500

    requests

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import requests
 import json
 i = 0
 data_user = []
-while i < 100:
+while i < 2:
     response = requests.get("https://random.dog/woof.json")
     if response.status_code == 200:
         data = response.json()
-        data_user.append(data)
+        data_user.append(data["url"])
     i += 1
-data_dog = json.dumps(data_user, indent=4)
+# data_dog = json.dumps(data_user, indent=4)
 
 with open("users.json", mode="w") as file:
-    file.write(data_dog)
+    file.write(data_user)
